02ac0f8dceac067d4f1047bbdec8e392f0fa93b0
3sky <3sky@protonmail.com>
2020-03-24 21:39:05 +0100

    try to resolve the issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,6 @@ PORT=$6
 IMAGE=$7
 ALLOW=$8
 
-# Make temp authfile
-echo $AUTH_FILE > /tmp/encode_auth.json
-cat /tmp/encode_auth.json
-cat /tmp/encode_auth.json | base64 --decode > /tmp/auth.json
-
 # Activate account
 if gcloud auth activate-service-account $EMAIL --key-file=/tmp/auth.json ; then
     echo "Authentication successful"
