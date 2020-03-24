03081eae9e9bfd7d1ddb38521deed7942c91c0da
3sky <3sky@protonmail.com>
2020-03-24 22:26:11 +0100

    try to resolve the issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,17 +12,16 @@ ALLOW=$8
 
 echo "$AUTH_FILE" | base64 --decode > /tmp/auth.json
 chmod 777 /tmp/auth.json
-cat /tmp/auth.json
 
 # Activate account
-if gcloud auth activate-service-account $EMAIL --key-file=/tmp/auth.json ; then
+if gcloud auth activate-service-account "$EMAIL" --key-file=/tmp/auth.json ; then
     echo "Authentication successful"
 else
     echo "Authentication faild"
     exit 1;
 fi
 
-if [[ "$ACTION_TYPE" =~ ^(run|update|delete)$ ]]; then
+if [ "$ACTION_TYPE" = "run" ] || [ "$ACTION_TYPE" = "update" ] || [ "$ACTION_TYPE" = "delete" ]; then
     echo "Choose $ACTION_TYPE as action type"
 else
     echo "Wrong action type, Possible solution run|update|delete"
@@ -35,22 +34,20 @@ else
     au=" "
 fi
 
-if [ "$ACTION_TYPE" == "run"]; then
-    gcloud run deploy $NAME \
+if [ "$ACTION_TYPE" = "run" ]; then
+    gcloud run deploy "$NAME" \
     --platform managed \
-    $au \
-    --region $REGION \
-    --port $PORT \
-    --image $IMAGE
-elif [ "$ACTION_TYPE" == "update"]; then
-    gcloud run deploy $NAME \
-    --project ${{ secrets.PROJECT_ID }} \  
+    "$au" \
+    --region "$REGION" \
+    --port "$PORT" \
+    --image "$IMAGE"
+elif [ "$ACTION_TYPE" = "update" ]; then
+    gcloud run deploy "$NAME" \
     --platform managed \
-    --region $REGION \ 
-    --image $IMAGE
-elif [ "$ACTION_TYPE" == "delete"]; then
-    gcloud run services delete $NAME \
+    --region "$REGION" \
+    --image "$IMAGE"
+elif [ "$ACTION_TYPE" = "delete" ]; then
+    gcloud run services delete "$NAME" \
     --platform managed \
-    --region $REGION  
+    --region "$REGION"  
 fi
-
